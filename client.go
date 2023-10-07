b00d7cd55cec6d1453859d445540b7f5416585f2
emcegom <emcegom@gmail.com>
2023-10-07 23:50:35 +0800

    client

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 	"strconv"
 )
 
@@ -24,13 +26,153 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
 	if err != nil {
-		fmt.Println("net.Dial err : " , err)
+		fmt.Println("net.Dial err : ", err)
 		return nil
 	}
 
 	client.conn = conn
 
 	return client
+}
+
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println(
+		`
+		1.公聊模式
+		2.私聊模式
+		3.更新用户名
+		0.退出
+		`,
+	)
+
+	fmt.Scan(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println("please input number in the range")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+
+		switch client.flag {
+		case 1:
+			client.PublicChat()
+		case 2:
+			client.PrivateChat()
+		case 3:
+			client.UpdateName()
+		}
+	}
+}
+
+func (client *Client) UpdateName() bool {
+	fmt.Println("please input a new username : ")
+
+	var newName string
+
+	fmt.Scanln(&newName)
+
+	sendMsg := "rename|" + newName + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return false
+	}
+	client.Name = newName
+	return true
+
+}
+
+func (client *Client) PublicChat() {
+	var chatMsg string
+
+	fmt.Println("please input your message or input exit to logout")
+
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("conn write err : ", err)
+				break
+			}
+
+		}
+
+		chatMsg = ""
+		fmt.Println("please input your message or input exit to logout")
+		fmt.Scanln(&chatMsg)
+	}
+
+}
+
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn write err : ", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var remoteName string
+
+	client.SelectUsers()
+
+	fmt.Println("please input the communication part, or input exit to logout")
+
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+
+		var chatMsg string
+
+		fmt.Println("please input your message or input exit to logout")
+
+		fmt.Scanln(&chatMsg)
+
+		for chatMsg != "exit" {
+
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn write err : ", err)
+					break
+				}
+
+			}
+
+			chatMsg = ""
+			fmt.Println("please input your message or input exit to logout")
+			fmt.Scanln(&chatMsg)
+		}
+
+		client.SelectUsers()
+
+		fmt.Println("please input the communication part, or input exit to logout")
+
+		fmt.Scanln(&remoteName)
+
+	}
+
+}
+
+func (client *Client) DealResponse() {
+	io.Copy(os.Stdout, client.conn)
 }
 
 var serverIp string
@@ -50,7 +192,10 @@ func main() {
 		fmt.Println("connecting server " + serverIp + ":" + strconv.Itoa(serverPort) + " fail")
 		return
 	}
+
+	go client.DealResponse()
+
 	fmt.Println("connecting server " + serverIp + ":" + strconv.Itoa(serverPort) + " success")
-	select{
-	}
+
+	client.Run()
 }
--- readme.md
+++ readme.md
@@ -4,4 +4,6 @@
 go run ./main.go ./server.go ./user.go
 
 telnet localhost 8888
+
+
 ```
